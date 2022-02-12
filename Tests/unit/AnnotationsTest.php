adff112fea9db7dfcb1106d3c02641a9dee96bbb
masoud harooni <masoudharooni50@gmail.com>
2022-02-12 15:42:07 +0330

    Depend annotation

---
+++ Tests/unit/AnnotationsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+
+class Annotations extends TestCase
+{
+    protected $value;
+    public function testCheckCorrectValue()
+    {
+        $this->value++;
+        $this->assertEquals(1, $this->value);
+        return $this->value;
+    }
+    /**
+     * @depends testCheckCorrectValue
+     */
+    public function testCheckCorrectValue2($value)
+    {
+        $value++;
+        $this->assertEquals(2, $value);
+    }
+}
